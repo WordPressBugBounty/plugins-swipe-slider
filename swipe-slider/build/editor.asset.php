b81f700355af11e0f9865dc1df7e1e592bac0e7b
Sean Murphy <sean@iamseanmurphy.com>
2024-12-17 16:18:54 +0000

    0.14

---
+++ swipe-slider/build/editor.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-i18n', 'wp-keyboard-shortcuts'), 'version' => '7c4bb8589ab316a329f2');
